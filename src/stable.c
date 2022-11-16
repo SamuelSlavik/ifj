dd4dfd265d0ebcb3f2677daf6c3db9a799e079d4
imiskii <las.michal00@gmail.com>
2022-11-16 15:20:17 +0100

    Symbol table functions init commit

---
+++ src/stable.c
@@ -0,0 +1,8 @@
+/**
+ * @file stable.c
+ * @author Michal Ľaš (xlasmi00)
+ * @brief 
+ * @version 0.1
+ * @date 2022-11-16
+ * 
+ */
---
+++ src/stable.h
@@ -0,0 +1,8 @@
+/**
+ * @file stable.h
+ * @author Michal Ľaš (xlasmi00)
+ * @brief 
+ * @version 0.1
+ * @date 2022-11-16
+ * 
+ */
